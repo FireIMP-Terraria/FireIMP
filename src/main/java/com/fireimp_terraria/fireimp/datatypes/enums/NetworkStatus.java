9995d76362942b271051939e4beaca64a58c5f09
Dylan Katz <dylanishappy1@gmail.com>
2016-03-13 23:48:16 -0600

    Added network status :fire:

---
+++ src/main/java/com/fireimp_terraria/fireimp/datatypes/enums/NetworkStatus.java
@@ -0,0 +1,8 @@
+package com.fireimp_terraria.fireimp.datatypes.enums;
+
+/**
+ * Created by Dylan on 3/13/2016.
+ */
+public enum NetworkStatus {
+    OK, LATER_VERSION, UNKNOWN_ERROR, EMPTY_FILE, DECRYPTION_ERROR, BAD_SECTION_POINTER, BAD_FOOTER
+}
