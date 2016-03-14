a224c7e428b9553adce4251a192124b83332d42a
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2016-03-14 23:42:26 +0100

    v0.0.1-dev1-SNAPSHOT

--- src/main/java/com/fireimp_terraria/fireimp/TerrariaServer.java
+++ src/main/java/com/fireimp_terraria/fireimp/TerrariaServer.java
@@ -3,7 +3,7 @@ package com.fireimp_terraria.fireimp;
 /**
  * Created by Dylan on 3/13/2016.
  */
-public class FireIMP {
+public class TerrariaServer {
     public static void main(String[] args) {
         //TODO: Actually make the server
     }
