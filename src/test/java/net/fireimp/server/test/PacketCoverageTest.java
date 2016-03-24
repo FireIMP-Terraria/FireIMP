b0098987c49d042c7743b70467edc841ee41f29d
Dylan Katz <dylanishappy1@gmail.com>
2016-03-24 10:57:49 -0600

    Packet coverage test
    Because why not?

---
+++ src/test/java/net/fireimp/server/test/PacketCoverageTest.java
@@ -0,0 +1,24 @@
+package net.fireimp.server.test;
+
+import net.fireimp.server.network.packets.PacketType;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PacketCoverageTest {
+    @Test
+    public void testPacketCoverage() {
+        double coverage = 0;
+        double total = 0;
+        for(PacketType type : PacketType.values()) {
+            Class<?> typeClass = PacketType.getClassByType(type);
+            if (typeClass != null) {
+                coverage += 1;
+            }
+            total += 1;
+        }
+        double percentage = (coverage/total) * 100;
+        System.out.println("Packet coverage: " + percentage + "% (" + coverage + "/" + total + "}");
+        Assert.assertTrue(true);
+
+    }
+}
